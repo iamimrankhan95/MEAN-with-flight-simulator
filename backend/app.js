0d2a09aca01a195242a1c8f7eaf6ee4c8d36224a
thanos <imran.khan@synesisit.com.bd>
2020-10-17 20:44:35 +0600

    improved server.js

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+// const port = process.env.PORT || port;
+// const www = process.env.WWW || './';
+// app.use(express.static(www));
+// console.log(`serving ${www}`);
+// app.get('*', (req, res) => {
+//   res.sendFile(`index.html`, { root: www });
+// });
+// app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+
+app.use((req,res,next)=>{
+  console.log('first middlewear');
+  next();
+})
+
+app.use((req,res,next)=>{
+  res.send('helllooooolllo');
+})
+
+module.exports = app;
+
