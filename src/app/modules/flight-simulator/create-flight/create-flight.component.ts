b4ef3d74ee8b87d73d5dee3e08ead945acdb047d
thanos <imran.khan@synesisit.com.bd>
2020-10-18 19:18:02 +0600

    ready create form

--- src/app/modules/flight-simulator/create-flight/create-flight.component.ts
+++ src/app/modules/flight-simulator/create-flight/create-flight.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FlightSimulatorHttpService } from '../flight-simulator-http.service';
+import { FlightSimulatorService } from '../flight-simulator.service';
 
 @Component({
   selector: 'app-create-flight',
@@ -7,9 +12,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateFlightComponent implements OnInit {
 
-  constructor() { }
+  today = new Date();
+  fromMinDate = { year: this.today.getFullYear() - 100, month: 1, day: 1 };
+  fromMaxDate = { year: this.today.getFullYear() + 100, month: 1, day: 1 };
+  isFlightFormSubmitted = false;
+
+  flightFrm = this.fb.group({
+    AirlineLogoAddress: [],
+    AirlineName: ['', [Validators.required]],
+    Stops: ['', [Validators.required]],
+    TotalAmount: ['', [Validators.required, Validators.pattern('^$|^[0-9]+')]],
+    DepartureAirportCode: ['',
+      [Validators.required, Validators.maxLength(3), Validators.minLength(3), Validators.pattern('^$|^[A-Za-z0-9]+')]],
+    ArrivalAirportCode: ['',
+      [Validators.required, Validators.maxLength(3), Validators.minLength(3), Validators.pattern('^$|^[A-Za-z0-9]+')]],
+    DepartureDate: ['', [Validators.required]],
+    ReturnDate: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService,
+    private flightSimulatorHttpService: FlightSimulatorHttpService,
+    private flightSimulatorService: FlightSimulatorService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  reset() {
+    this.isFlightFormSubmitted = false;
+    this.flightFrm.reset();
+  }
+
+  onSubmit() {
+    this.isFlightFormSubmitted = true;
+    if (!this.flightFrm.valid) {
+      this.toastr.error('Form validation failed', 'Error');
+      console.log('not valid', this.flightFrm);
+      return;
+    }
+
+    console.log('valid', this.flightFrm.value);
+
+    const flightFormValue = this.flightFrm.value;
+    // this.flightSimulatorHttpService.getFlightSimulatorResponseObjects(flightFormValue)
+    //   .subscribe(
+    //     (flights: FlightSimulatorResponseObject[]) => {
+    //       this.flightSimulatorService.flights = flights;
+    //       this.router.navigate(['/list', {
+    //         queryParams: {
+    //           DepartureAirportCode: flightFormValue.DepartureAirportCode,
+    //           ArrivalAirportCode: flightFormValue.ArrivalAirportCode,
+    //           DepartureDate: flightFormValue.DepartureDate,
+    //           ReturnDate: flightFormValue.DepartureDate,
+    //         }
+    //       }]);
+    //     }
+    //   );
+
+    this.router.navigate(['list'], {
+      relativeTo: this.route,
+      queryParams: {
+        DepartureAirportCode: flightFormValue.DepartureAirportCode,
+        ArrivalAirportCode: flightFormValue.ArrivalAirportCode,
+        DepartureDate: flightFormValue.DepartureDate,
+        ReturnDate: flightFormValue.DepartureDate,
+      }
+    });
   }
 
 }
--- src/app/modules/flight-simulator/flight-simulator-routing.module.ts
+++ src/app/modules/flight-simulator/flight-simulator-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FlightSimulatorComponent } from './flight-simulator-form/flight-simulator.component';
 import { FlightListComponent } from './flight-list/flight-list.component';
+import { CreateFlightComponent } from './create-flight/create-flight.component';
 
 const routes: Routes = [
   { path: '', component: FlightSimulatorComponent },
   { path: 'list', component: FlightListComponent },
+  { path: 'create', component: CreateFlightComponent },
 ];
 
 @NgModule({
