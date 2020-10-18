6d45c089948515e7d1e19a00e7aa9394362824dc
thanos <imran.khan@synesisit.com.bd>
2020-10-18 15:39:01 +0600

    change url for flight

--- src/app/shared/enums/application-urls.ts
+++ src/app/shared/enums/application-urls.ts
@@ -2,6 +2,6 @@ import { environment } from '../../../environments/environment';
 
 export const applicationUrl = {
   flightSimulator: {
-    read: 'http://nmflightapi.azurewebsites.net/api/flight',
+    read: 'assets/flights.json',
   }
 };
